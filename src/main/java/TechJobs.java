6da0188fb88ad78d08ba56f662883346a66c35bb
Kyle Stadler <kstad4@gmail.com>
2022-09-26 21:51:39 -0500

    First Two Tasks Completed, Using Git for Testing

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,36 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.isEmpty()) {
+            System.out.println("No Results");
+        } else {
 
-        System.out.println("printJobs is not implemented yet");
+// [{},{},{}...]
+        for (HashMap<String, String> singleJob : someJobs) {
+            System.out.println("*****");
+
+            for (Map.Entry<String, String> jobDetail : singleJob.entrySet()) {
+                System.out.println(jobDetail.getKey() + ": " + jobDetail.getValue());
+            }
+            System.out.println("*****\n");
+        }
+
+
+
+
+
+        }
+
+
+/**Original data order on job_data.csv:
+name,employer,location,position type,core competency
+**/
+
+
+/**New order when calling Jobdata.findall()
+{position type=Data Scientist / Business Intelligence, name=Junior Data Analyst, employer=Lockerdome, location=Saint Louis, core competency=Statistical Analysis}
+*/
+
+//            System.out.println(someJobs);
     }
 }
